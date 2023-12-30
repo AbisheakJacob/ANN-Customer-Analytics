881f48fe0ff6f5bc961a7e1033455b3211daff09
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2023-12-30 20:38:21 +0530

    about page changes

--- pages/2_About.py
+++ pages/2_About.py
@@ -1,10 +1,7 @@
-import sys
-sys.path.insert(0, '../eda/profiler.py')
-
 import streamlit as st
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
-from eda.profiler import profile_report
+from ydata_profiling import ProfileReport
 
 
 # Set page configuration to wide layout
@@ -13,8 +10,15 @@ st.set_page_config(page_title="About", layout="wide")
 st.title("Exploratory Data Analysis")
 
 st.subheader("Dataset")
-# call the profile function to get the profile of the data
-data, profile = profile_report()
+
+# reading the data
+data = pd.read_csv("../data/Churn_Modelling.csv")
+
+# drop the unnecessary coloumns
+data.drop(columns=['RowNumber', 'CustomerId', 'name'], inplace=True)
+
+# create the profile report of the dataframe
+profile = ProfileReport(data, title="Profiling Report", explorative=True)
 
 st.dataframe(data)
 
