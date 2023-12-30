55c61b210263de3253ba6a6e39deadf812cf67b4
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2023-12-30 23:24:25 +0530

    ydata changes

--- pages/2_About.py
+++ pages/2_About.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #from streamlit_pandas_profiling import st_profile_report
-#from ydata_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 
 
 # Set page configuration to wide layout
@@ -18,7 +18,7 @@ data = pd.read_csv("data/Churn_Modelling.csv")
 data.drop(columns=['RowNumber', 'CustomerId', 'name'], inplace=True)
 
 # create the profile report of the dataframe
-#profile = ProfileReport(data, title="Profiling Report", explorative=True)
+profile = ProfileReport(data, title="Profiling Report", explorative=True)
 
 st.dataframe(data)
 
