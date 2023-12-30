b863f2e8275b9946b0223c10bc65d6e36b08d279
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2023-12-30 23:22:16 +0530

    about page

--- pages/2_About.py
+++ pages/2_About.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from streamlit_pandas_profiling import st_profile_report
-from ydata_profiling import ProfileReport
+#from streamlit_pandas_profiling import st_profile_report
+#from ydata_profiling import ProfileReport
 
 
 # Set page configuration to wide layout
@@ -12,14 +12,14 @@ st.title("Exploratory Data Analysis")
 st.subheader("Dataset")
 
 # reading the data
-data = pd.read_csv("../data/Churn_Modelling.csv")
+data = pd.read_csv("data/Churn_Modelling.csv")
 
 # drop the unnecessary coloumns
 data.drop(columns=['RowNumber', 'CustomerId', 'name'], inplace=True)
 
 # create the profile report of the dataframe
-profile = ProfileReport(data, title="Profiling Report", explorative=True)
+#profile = ProfileReport(data, title="Profiling Report", explorative=True)
 
 st.dataframe(data)
 
-st_profile_report(profile)
+#st_profile_report(profile)
